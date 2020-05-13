f4aea329a205836d96b9543dc59f96ad454c7f40
kian waters <kianwaters@gmail.com>
2020-05-13 10:40:41 +0100

    Step 2

---
+++ Kianwaters_s00187504/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kianwaters_s00187504
+{
+    public class Phone
+    {
+        //Properties
+        //Ex2
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_string { get; set; }
+    }
+}
