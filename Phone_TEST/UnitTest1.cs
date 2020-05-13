76876fe33f80d6785f5223a345ff921c62c1d746
kian waters <kianwaters@gmail.com>
2020-05-13 11:11:38 +0100

    Question 4

---
+++ Phone_TEST/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Kianwaters_s00187504;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Phone_TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IncreasePrice()
+        {
+            //Ex4
+            //Arrange
+            Phone phone = new Phone();
+            double expectedPrice = 500;
+
+            //Act
+            //Declaring the start price and the percentage value
+            phone.IncreasePrice(500, 10);
+
+            //Assert
+            Assert.AreEqual(expectedPrice, phone.Price);
+        }
+    }
+}
